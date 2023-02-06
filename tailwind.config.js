4d84b18884c71ebfa33450ef0e3ba5fb96503b99
kumarananda <coderanandakumar@gmail.com>
2023-02-06 18:11:12 +0600

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: "media", // media (divice theams) or class (state control as user )
   theme: {
     extend: {},
   },
